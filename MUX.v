73213dcf71ff4a1e1bdfc076b73e4fb3c92f2b67
Crasher1225 <deonandanajay@gmail.com>
2022-01-21 19:14:04 -0500

    Update MUX.v

--- MUX.v
+++ MUX.v
@@ -1,20 +1,20 @@
 module Mux;
-  	//Defining registers, wire and variables
-	reg a, b, c;
-	wire z, notOutput, Aout1, Aout2;
+    //Defining registers, wire and variables
+    reg a, b, c;
+    wire z, notOutput, Aout1, Aout2;
     integer i, j, k, expect;
   
     //Creating the Multiplexor circuit
-  	//Not gate result stored to notOuput
+    //Not gate result stored to notOuput
     not my_not(notOutput,c);
-  	//And gates results stored to Aout1 and Aout2
- 	and my_and(Aout1,a,notOutput);
-  	and my_and2(Aout2,c,b);
-  	//Or gate result stored to z
+    //And gates results stored to Aout1 and Aout2
+    and my_and(Aout1,a,notOutput);
+    and my_and2(Aout2,c,b);
+    //Or gate result stored to z
     or my_or(z,Aout1,Aout2);
     
   
-  	//Loop to test every input of reg's a, b and c
+    //Loop to test every input of reg's a, b and c
     initial
     begin
         for(i=0; i<2; i=i+1)
